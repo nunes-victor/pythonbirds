07180848b8fef2ca182890f602779773fb9a3ac5
nunes-victor <victor.a.n@gmail.com>
2020-08-03 10:49:53 -0300

    Implementado método de colisão.

--- atores.py
+++ atores.py
@@ -9,7 +9,7 @@ ATIVO = 'Ativo'
 GRAVIDADE = 10  # m/s^2
 
 
-class Ator():
+class Ator:
     """
     Classe que representa um ator. Ele representa um ponto cartesiano na tela.
     """
@@ -52,7 +52,11 @@ class Ator():
         :param intervalo: Intervalo a ser considerado
         :return:
         """
-        pass
+        if self.status==ATIVO and outro_ator.status==ATIVO:
+            delta_x = abs(self.x - outro_ator.x)
+            delta_y = abs(self.y - outro_ator.y)
+            if delta_x<=intervalo and delta_y<=intervalo:
+                self.status = outro_ator.status = DESTRUIDO
 
 
 
@@ -117,7 +121,7 @@ class Passaro(Ator):
         :param tempo: tempo de jogo a ser calculada a posição
         :return: posição x, y
         """
-        return 1, 1
+        return self.x, self.y
 
 
     def lancar(self, angulo, tempo_de_lancamento):
